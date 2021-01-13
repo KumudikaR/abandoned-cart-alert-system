8af472db4b189a356af1328b096d95b812986dbb
KumudikaR <kumudikarupasinghe@gmail.com>
2021-01-13 11:39:05 +0530

    alert system

---
+++ add-db-entry.py
@@ -0,0 +1,31 @@
+import boto3
+ddb = boto3.client("dynamodb")
+
+def handler(event, context):
+    
+    name = event['Name']
+    email = event['Email']
+    url = event['URL']
+
+    try:
+        data = ddb.put_item(
+            TableName="history",
+            Item={
+                'Name': {
+                    'S': name
+                },
+                'Email': {
+                    'S': email
+                },
+                'URL': {
+                    'S': url
+                }
+            }
+        )
+        print("Successfully Saved entry from email:"+ email)
+
+    except BaseException as e:
+        print(e)
+        raise(e)
+   
+    return {"message": "Successfully executed"}
